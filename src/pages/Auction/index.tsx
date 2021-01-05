278ece14155b67088625e356145e934e38d417df
josojo <josojo@hotmail.de>
2021-01-05 08:52:13 +0100

    Hack: enforce connecting with wallet

--- src/pages/Auction/index.tsx
+++ src/pages/Auction/index.tsx
@@ -14,9 +14,14 @@ import ClaimerBody from "../ClaimerBody";
 
 import AuctionDetails from "../../components/AuctionDetails";
 import AuctionHeader from "../../components/AuctionHeader";
+import { ButtonLight } from "../../components/Button";
+import { useActiveWeb3React } from "../../hooks";
+import { useWalletModalToggle } from "../../state/application/hooks";
 
 export default function Auction({ location: { search } }: RouteComponentProps) {
   useDefaultsFromURLSearch(search);
+  const { account } = useActiveWeb3React();
+  const toggleWalletModal = useWalletModalToggle();
 
   // swap state
   const { auctionId } = useSwapState();
@@ -25,41 +30,47 @@ export default function Auction({ location: { search } }: RouteComponentProps) {
   return (
     <>
       <AppBody>
-        <div style={{ padding: "1rem" }}>
-          <AuctionHeader></AuctionHeader>
-        </div>
-        <div
-          style={{
-            width: "35%",
-            float: "left",
-            alignContent: "center",
-            padding: "0.5rem",
-          }}
-        >
-          <AuctionDetails></AuctionDetails>
-        </div>
-        <div
-          style={{
-            width: "65%",
-            float: "right",
-            alignContent: "right",
-            padding: "0.5rem",
-          }}
-        >
-          {auctionEndDate >= new Date().getTime() / 1000 ? (
-            <OrderBody>
-              <Wrapper id="auction-page">
-                <OrderPlacement></OrderPlacement>
-              </Wrapper>
-            </OrderBody>
-          ) : (
-            <ClaimerBody>
-              <Wrapper id="auction-page">
-                <Claimer></Claimer>
-              </Wrapper>
-            </ClaimerBody>
-          )}
-        </div>
+        {!account ? (
+          <ButtonLight onClick={toggleWalletModal}>Connect Wallet</ButtonLight>
+        ) : (
+          <div>
+            <div style={{ padding: "1rem" }}>
+              <AuctionHeader></AuctionHeader>
+            </div>
+            <div
+              style={{
+                width: "35%",
+                float: "left",
+                alignContent: "center",
+                padding: "0.5rem",
+              }}
+            >
+              <AuctionDetails></AuctionDetails>
+            </div>
+            <div
+              style={{
+                width: "65%",
+                float: "right",
+                alignContent: "right",
+                padding: "0.5rem",
+              }}
+            >
+              {auctionEndDate >= new Date().getTime() / 1000 ? (
+                <OrderBody>
+                  <Wrapper id="auction-page">
+                    <OrderPlacement></OrderPlacement>
+                  </Wrapper>
+                </OrderBody>
+              ) : (
+                <ClaimerBody>
+                  <Wrapper id="auction-page">
+                    <Claimer></Claimer>
+                  </Wrapper>
+                </ClaimerBody>
+              )}
+            </div>
+          </div>
+        )}
       </AppBody>
     </>
   );
