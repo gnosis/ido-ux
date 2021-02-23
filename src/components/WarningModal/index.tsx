b595c6f55d5a5776f8e6da5eed36b81f297eb756
Jose Ramirez <jarcodallo@gmail.com>
2021-02-23 17:07:03 -0300

    remove unset variables

--- src/components/WarningModal/index.tsx
+++ src/components/WarningModal/index.tsx
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import styled, { ThemeContext } from "styled-components";
+import React from "react";
+import styled from "styled-components";
 import { Text } from "rebass";
 import { RowBetween } from "../Row";
 import { CloseIcon } from "../../theme/components";
