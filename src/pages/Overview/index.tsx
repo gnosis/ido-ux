d2d5be5e47f17edcd0660bc56beefc5e1e6da23f
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2021-02-26 10:10:37 -0300

    (fix) make the auctions page less messy

--- src/pages/Overview/index.tsx
+++ src/pages/Overview/index.tsx
@@ -23,54 +23,9 @@ const ViewBtn = styled(ButtonLight)`
     margin: 0 0 0 5px;
   }
 `
-const Wrapper = styled.div`
-  ${({ theme }) => theme.mediaWidth.upToMedium`flex-flow: column wrap;`};
-  display: flex;
-  flex-flow: row wrap;
-  height: 100%;
-  justify-content: space-between;
-  padding: 1rem;
-  width: 100%;
-`
-
-const TableWrapper = styled.div`
-  display: block;
-  position: relative;
-  max-width: 700px;
-  width: 100%;
-  flex: 0 1 auto;
-  box-sizing: border-box;
-  box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.01), 0px 4px 8px rgba(0, 0, 0, 0.04),
-    0px 16px 24px rgba(0, 0, 0, 0.04), 0px 24px 32px rgba(0, 0, 0, 0.01);
-  border-radius: 20px;
-  padding: 16px;
-  align-items: center;
-
-  > div {
-    width: 100%;
-  }
-`
-
-const ThemeHeader = styled.div`
-  display: flex;
-  flex-flow: row wrap;
-  padding: 0.5rem;
-  min-width: 300px;
-  min-height: 20px;
-  align-items: stretch;
-  border-radius: 40px;
-  background: ${({ theme }) => theme.bg1};
-`
-
-const TitleText = styled.div`
-  font-size: 33px;
-  font-weight: 900;
-  padding: 5rem;
-  text-align: center;
-`
 
 export default function Overview() {
-  // We should think about how to get a network id without connection to metamaks
+  // We should think about how to get a network id without connection to metamask
   const chainId = 4
   const highlightedAuctions = useInterestingAuctionInfo(4, chainId)
   const allAuctions = useAllAuctionInfo()
@@ -107,25 +62,12 @@ export default function Overview() {
   })
   return (
     <>
-      <ThemeHeader>
-        <TitleText>GnosisAuction - fairest auctions</TitleText>
-      </ThemeHeader>
-      <TableWrapper>
-        <Wrapper>
-          <h3>Highlighted auctions:</h3>
-        </Wrapper>
-        <Wrapper>
-          {Object.entries(highlightedAuctions).map((auctionInfo) => (
-            <AuctionInfoCard key={auctionInfo[0]} {...auctionInfo[1]} />
-          ))}
-        </Wrapper>
-      </TableWrapper>
-      <TableWrapper>
-        <Wrapper>
-          <h3>All auctions:</h3>
-        </Wrapper>
-        <DatatablePage {...tableData} />
-      </TableWrapper>
+      <div style={{ display: 'flex', marginBottom: '40px' }}>
+        {Object.entries(highlightedAuctions).map((auctionInfo) => (
+          <AuctionInfoCard key={auctionInfo[0]} {...auctionInfo[1]} />
+        ))}
+      </div>
+      <DatatablePage {...tableData} />
     </>
   )
 }
