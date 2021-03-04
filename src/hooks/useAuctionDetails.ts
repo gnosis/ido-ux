ef224ef261d916d85b00c0875af98fb2d970dcea
Mariano Aguero <mariano.aguero@gmail.com>
2021-03-04 07:52:39 -0300

    Add new endpoint for auctions (#129)
    * Add endpoint to fetch auction details
    
    * Add hook to fetch auction details
    
    * Fix bad use of useMemo
    
    * Refactoring
    
    * Update hook to use the new endpoint for auction details
    
    * Apply update to use auctionStartDate and a setInterval instead of setTimeout

---
+++ src/hooks/useAuctionDetails.ts
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react'
+
+import { additionalServiceApi } from '../api'
+import { PricePoint } from '../api/AdditionalServicesApi'
+import { useActiveWeb3React } from './index'
+
+export interface AuctionInfoDetail {
+  auctionId: number
+  order: PricePoint
+  symbolAuctioningToken: string
+  symbolBiddingToken: string
+  addressAuctioningToken: string
+  addressBiddingToken: string
+  decimalsAuctioningToken: number
+  decimalsBiddingToken: number
+  endTimeTimestamp: number
+  startingTimestamp: number
+  chainId: String
+  minimumBiddingAmountPerOrder: string
+  exactOrder: string
+}
+
+export const useAuctionDetails = (auctionId: number): Maybe<AuctionInfoDetail> => {
+  const { chainId } = useActiveWeb3React()
+
+  const [auctionInfo, setAuctionInfo] = useState<Maybe<AuctionInfoDetail>>(null)
+
+  useEffect(() => {
+    let cancelled = false
+
+    const fetchApiData = async () => {
+      try {
+        if (!chainId) {
+          throw new Error('missing dependencies in useAuctionDetails callback')
+        }
+
+        const params = {
+          networkId: chainId,
+          auctionId,
+        }
+
+        const auctionInfo = await additionalServiceApi.getAuctionDetails(params)
+        if (!cancelled) {
+          setAuctionInfo(auctionInfo)
+        }
+      } catch (error) {
+        setAuctionInfo(null)
+        console.error('Error getting auction details', error)
+      }
+    }
+    fetchApiData()
+
+    return (): void => {
+      cancelled = true
+    }
+  }, [setAuctionInfo, auctionId, chainId])
+
+  return auctionInfo
+}
