cd37ce449a21aa4ca85fdc3f432efdc6c12cd429
Mariano Aguero <mariano.aguero@gmail.com>
2021-03-24 20:08:15 -0300

    Solve performance problem between the chart and the order placement (#297)
    * Add improvement to loading on the auction details section
    
    * Add check to not reload the chart for the moment, causes a downgrade in the performance
    
    * Add check for existence of userBalance

--- src/components/auction/OrderPlacement/index.tsx
+++ src/components/auction/OrderPlacement/index.tsx
@@ -267,7 +267,7 @@ const OrderPlacement: React.FC<OrderPlacementProps> = (props) => {
                 Your Balance:{' '}
                 <Total>{`${
                   account
-                    ? `${userTokenBalance?.toSignificant(6)} ${
+                    ? `${userTokenBalance && userTokenBalance?.toSignificant(6)} ${
                         derivedAuctionInfo?.biddingToken?.symbol
                       }`
                     : 'Connect your wallet'
