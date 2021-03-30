ecbe991a4e302ad372961c20f82b97e86f8bbcfd
Mariano Aguero <mariano.aguero@gmail.com>
2021-03-30 14:04:20 -0300

    Add reset modal on dismiss warning

--- src/components/auction/OrdersTable/index.tsx
+++ src/components/auction/OrdersTable/index.tsx
@@ -269,6 +269,7 @@ const OrderTable: React.FC<OrderTableProps> = (props) => {
             content={orderError}
             isOpen={showWarning}
             onDismiss={() => {
+              resetModal()
               setOrderError(null)
               setShowWarning(false)
             }}
