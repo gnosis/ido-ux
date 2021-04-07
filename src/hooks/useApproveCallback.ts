707e1c2f053d31af87eb9c9cc6ff414778d488fa
Jose A. Ramirez C <jarcodallo@gmail.com>
2021-04-07 15:38:32 -0300

    [issue-379] xdai gas price is not always set correctly (#435)
    * fix xdai gas price is not always set correctly
    
    * add useGasPrice hook
    
    * review

--- src/hooks/useApproveCallback.ts
+++ src/hooks/useApproveCallback.ts
@@ -10,6 +10,7 @@ import { calculateGasMargin } from '../utils'
 import { getLogger } from '../utils/logger'
 import { useActiveWeb3React } from './index'
 import { useTokenContract } from './useContract'
+import { useGasPrice } from './useGasPrice'
 
 const logger = getLogger('useApproveCallback')
 
@@ -26,6 +27,7 @@ export function useApproveCallback(
   addressToApprove?: string,
 ): [ApprovalState, () => Promise<void>] {
   const { account } = useActiveWeb3React()
+  const gasPrice = useGasPrice()
 
   const currentAllowance = useTokenAllowance(
     amountToApprove?.token,
@@ -77,6 +79,7 @@ export function useApproveCallback(
 
     return tokenContract
       .approve(addressToApprove, useExact ? amountToApprove.raw.toString() : MaxUint256, {
+        gasPrice,
         gasLimit: calculateGasMargin(estimatedGas),
       })
       .then((response: TransactionResponse) => {
@@ -89,7 +92,7 @@ export function useApproveCallback(
         logger.debug('Failed to approve token', error)
         throw error
       })
-  }, [approval, tokenContract, addressToApprove, amountToApprove, addTransaction])
+  }, [approval, tokenContract, amountToApprove, addressToApprove, gasPrice, addTransaction])
 
   return [approval, approve]
 }
