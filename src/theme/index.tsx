82bf34ced0f1931fa96b9ebd8abd1dc007151b03
Gabito Esmiapodo <gabriel@bootnode.dev>
2021-05-05 13:55:54 -0300

    (revert) progress circle background color to how it was before

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -14,9 +14,9 @@ export * from './components'
 
 const MEDIA_WIDTHS = {
   upToExtraSmall: 500,
-  upToSmall: 600,
-  upToMedium: 960,
   upToLarge: 1280,
+  upToMedium: 960,
+  upToSmall: 600,
 }
 
 const mediaWidthTemplates: {
@@ -50,7 +50,7 @@ export function colors(darkMode: boolean): Colors {
     modalBG: darkMode ? 'rgba(0,0,0,0.85)' : 'rgba(0,0,0,0.6)',
     primary1: darkMode ? '#e8663d' : '#e8663d',
     primary2: darkMode ? '#0052af' : '#0052af',
-    primary3: darkMode ? '#522420' : '#522420',
+    primary3: darkMode ? '#9f482d' : '#9f482d',
     primary4: darkMode ? '#001E3C' : '#001E3C',
     primary5: darkMode ? 'rgba(69,104,255,0.25)' : 'rgba(69,104,255,0.25)',
     primaryText1: darkMode ? '#6F9DFF' : '#3F77FF',
