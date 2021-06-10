071e5c40ec314b1090dd818bb5c4de5ffb588acd
Ramiro Vazquez <ramirovazquez@gmail.com>
2021-06-10 10:28:44 -0300

    disable react-scrollbar to prevent unneeded renders

--- src/components/navigation/Routes/Routes.tsx
+++ src/components/navigation/Routes/Routes.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Redirect, Route, RouteComponentProps, Switch, withRouter } from 'react-router-dom'
 import styled from 'styled-components'
 
-import ScrollArea from 'react-scrollbar'
+//import ScrollArea from 'react-scrollbar'
 import ReactTooltip from 'react-tooltip'
 
 import Auction from '../../../pages/Auction'
@@ -59,63 +59,56 @@ const Routes: React.FC<RouteComponentProps> = (props) => {
         textColor="#fff"
       />
       {showTopWarning && <TopDisclaimer />}
-      <ScrollArea smoothScrolling speed={0.8}>
-        <MainScroll>
-          <span id="topAnchor" />
-          <Inner>
-            <Web3ReactManager>
-              <Switch>
-                <Route
-                  exact
-                  path="/auction"
-                  render={(props) => <Auction showTokenWarning={tokenSupport} {...props} />}
-                  strict
-                />
-                <Route component={Overview} exact path="/overview" strict />
-                <Route component={Landing} exact path="/start" strict />
-                <Route component={Terms} exact path="/terms-and-conditions" strict />
-                <Route component={Licenses} exact path="/licenses" strict />
-                <Route component={Documentation} exact path="/docs" strict />
-                <Route component={Documentation} exact path="/docs/batch-auctions" strict />
-                <Route component={Documentation} exact path="/docs/use-cases" strict />
-                <Route component={Documentation} exact path="/docs/user-flow" strict />
-                <Route
-                  component={Documentation}
-                  exact
-                  path="/docs/participate-as-a-bidder"
-                  strict
-                />
-                <Route
-                  component={Documentation}
-                  exact
-                  path="/docs/participate-as-auctioneer"
-                  strict
-                />
-                <Route
-                  component={Documentation}
-                  exact
-                  path="/docs/starting-an-auction-with-safe"
-                  strict
-                />
-                <Route
-                  component={Documentation}
-                  exact
-                  path="/docs/Private-Auctions-And-KYC-solutions"
-                  strict
-                />
-                <Route component={Documentation} exact path="/docs/faq" strict />
-                <Route exact path="/">
-                  <Redirect to="/start" />
-                </Route>
-                <Route path="*">
-                  <BaseCard>Page not found Error 404</BaseCard>
-                </Route>
-              </Switch>
-            </Web3ReactManager>
-          </Inner>
-          <Footer />
-        </MainScroll>
-      </ScrollArea>
+      <MainScroll>
+        <span id="topAnchor" />
+        <Inner>
+          <Web3ReactManager>
+            <Switch>
+              <Route
+                exact
+                path="/auction"
+                render={(props) => <Auction showTokenWarning={tokenSupport} {...props} />}
+                strict
+              />
+              <Route component={Overview} exact path="/overview" strict />
+              <Route component={Landing} exact path="/start" strict />
+              <Route component={Terms} exact path="/terms-and-conditions" strict />
+              <Route component={Licenses} exact path="/licenses" strict />
+              <Route component={Documentation} exact path="/docs" strict />
+              <Route component={Documentation} exact path="/docs/batch-auctions" strict />
+              <Route component={Documentation} exact path="/docs/use-cases" strict />
+              <Route component={Documentation} exact path="/docs/user-flow" strict />
+              <Route component={Documentation} exact path="/docs/participate-as-a-bidder" strict />
+              <Route
+                component={Documentation}
+                exact
+                path="/docs/participate-as-auctioneer"
+                strict
+              />
+              <Route
+                component={Documentation}
+                exact
+                path="/docs/starting-an-auction-with-safe"
+                strict
+              />
+              <Route
+                component={Documentation}
+                exact
+                path="/docs/Private-Auctions-And-KYC-solutions"
+                strict
+              />
+              <Route component={Documentation} exact path="/docs/faq" strict />
+              <Route exact path="/">
+                <Redirect to="/start" />
+              </Route>
+              <Route path="*">
+                <BaseCard>Page not found Error 404</BaseCard>
+              </Route>
+            </Switch>
+          </Web3ReactManager>
+        </Inner>
+        <Footer />
+      </MainScroll>
       <CookiesBanner
         isBannerVisible={showCookiesBanner}
         onHide={() => {
