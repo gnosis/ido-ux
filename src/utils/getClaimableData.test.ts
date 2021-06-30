316e9000ee5c58284ab4cd3db10fded1aaace88b
Maximiliano Molina <hpmaxi@gmail.com>
2021-06-30 18:24:29 -0300

    Add failing test

--- src/utils/getClaimableData.test.ts
+++ src/utils/getClaimableData.test.ts
@@ -5,22 +5,19 @@ import { BigNumber } from '@ethersproject/bignumber'
 import { encodeOrder } from '../hooks/Order'
 import { getClaimableData } from '../hooks/useClaimOrderCallback'
 
-const auctioningToken = new Token(
+const weth = new Token(
   ChainId.RINKEBY,
-  '0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b',
-  6,
+  '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+  18,
   'USDC',
   '',
 )
-const biddingToken = new Token(
-  ChainId.RINKEBY,
-  '0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa',
-  18,
-  'KNC',
-  '',
-)
 
-describe('getClaimableData', async () => {
+const usdc = new Token(ChainId.RINKEBY, '0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b', 6, 'USDC', '')
+
+const dai = new Token(ChainId.RINKEBY, '0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa', 18, 'DAI', '')
+
+describe('getClaimableData', () => {
   it('checks that participant receives all their biddingTokens back if minFundingThreshold was not met', () => {
     const sellOrders = [
       {
@@ -38,8 +35,8 @@ describe('getClaimableData', async () => {
     }
 
     const claimed = getClaimableData({
-      auctioningToken,
-      biddingToken,
+      auctioningToken: usdc,
+      biddingToken: dai,
       clearingPriceVolume,
       clearingPriceOrder,
       minFundingThresholdNotReached: true,
@@ -47,8 +44,40 @@ describe('getClaimableData', async () => {
     })
 
     expect(claimed).toStrictEqual({
-      claimableAuctioningToken: new TokenAmount(auctioningToken, '0'),
-      claimableBiddingToken: new TokenAmount(biddingToken, '10000000'),
+      claimableAuctioningToken: new TokenAmount(usdc, '0'),
+      claimableBiddingToken: new TokenAmount(dai, '10000000'),
+    })
+  })
+
+  it('checks that participant receives auctioning tokens back if minFundingThreshold is met', () => {
+    const sellOrders = [
+      {
+        sellAmount: BigNumber.from('100000000000000000'),
+        buyAmount: BigNumber.from('149925037481259370314'),
+        userId: BigNumber.from(2),
+      },
+    ]
+    const ordersFromUser = sellOrders.map((o) => encodeOrder(o))
+    const clearingPriceVolume = BigNumber.from('10005000000000000')
+    const clearingPriceOrder = {
+      sellAmount: BigNumber.from('100000000000000000'),
+      buyAmount: BigNumber.from('149925037481259370314'),
+      userId: BigNumber.from(2),
+    }
+
+    const claimed = getClaimableData({
+      auctioningToken: weth,
+      biddingToken: dai,
+      clearingPriceVolume,
+      clearingPriceOrder,
+      minFundingThresholdNotReached: true,
+      ordersFromUser,
+    })
+
+    // 0 and 100
+    expect(claimed).toStrictEqual({
+      claimableAuctioningToken: new TokenAmount(weth, '0'),
+      claimableBiddingToken: new TokenAmount(dai, '100'),
     })
   })
 })
