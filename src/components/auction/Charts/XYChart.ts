a2f08d0b70edda7f5b4c7a04712dbd24f8d9a571
Maria Yablonskaya <mariayablonskaya@MacBook-Pro-Maria.local>
2021-08-30 07:07:51 +0300

    chart legend redraw

--- src/components/auction/Charts/XYChart.ts
+++ src/components/auction/Charts/XYChart.ts
@@ -5,8 +5,11 @@ import * as am4core from '@amcharts/amcharts4/core'
 import am4themesSpiritedaway from '@amcharts/amcharts4/themes/spiritedaway'
 import { Decimal } from 'decimal.js'
 
+import bidsPic from '../../../assets/images/bidsLegendItem.svg'
+import currPricePic from '../../../assets/images/currPriceLegendItem.svg'
+import newOrderPic from '../../../assets/images/newOrderLegendItem.svg'
+import sellSupplyPic from '../../../assets/images/sellSupplyLegendItem.svg'
 import { ChainId, getTokenDisplay } from '../../../utils'
-
 // Recalculates very big and very small numbers by reducing their length according to rules and applying suffix/prefix.
 const numberFormatter = new am4core.NumberFormatter()
 numberFormatter.numberFormat = '###.00 a'
@@ -50,9 +53,34 @@ export const XYChart = (props: XYChartProps): am4charts.XYChart => {
     blue: '#174172',
   }
 
+  // Legend
+  chart.legend = new am4charts.Legend()
+  chart.legend.position = 'bottom'
+  chart.legend.marginBottom = 15
+  chart.legend.marginTop = 10
+
+  chart.legend.useDefaultMarker = true
+  /* Remove square from marker template */
+  const marker = chart.legend.markers.template
+  marker.disposeChildren()
+  marker.width = 23
+  marker.height = 23
+
+  /* Add custom image instead */
+  const flag = marker.createChild(am4core.Image)
+  flag.width = 23
+  flag.height = 23
+  flag.verticalCenter = 'top'
+  flag.horizontalCenter = 'left'
+  flag.adapter.add('href', (href: any, target: any) => {
+    if (target.dataItem && target.dataItem.dataContext && target.dataItem.dataContext.dummyData) {
+      return target.dataItem.dataContext.dummyData.flag
+    } else {
+      return href
+    }
+  })
+
   // Create axes
-  console.log(chart.yAxes, chart)
-  const valueAxis = chart.yAxes.push(new am4charts.ValueAxis())
   const priceAxis = chart.xAxes.push(new am4charts.ValueAxis())
   const volumeAxis = chart.yAxes.push(new am4charts.ValueAxis())
   volumeAxis.renderer.grid.template.stroke = am4core.color(colors.white)
@@ -79,12 +107,9 @@ export const XYChart = (props: XYChartProps): am4charts.XYChart => {
   bidSeries.dataFields.valueX = 'priceNumber'
   bidSeries.dataFields.valueY = 'bidValueY'
   bidSeries.name = 'Bids'
-  bidSeries.strokeWidth = 1
   bidSeries.stroke = am4core.color(colors.green)
-  bidSeries.fill = bidSeries.stroke
-  bidSeries.startLocation = 0.5
-  bidSeries.fillOpacity = 0.1
   bidSeries.dummyData = {
+    flag: bidsPic,
     description:
       'Shows the price (x axis) and size (y axis)<br/> of the bids that have been placed,<br/> both expressed in the bid token',
   }
@@ -93,16 +118,12 @@ export const XYChart = (props: XYChartProps): am4charts.XYChart => {
   const askSeries = chart.series.push(new am4charts.LineSeries())
   askSeries.dataFields.valueX = 'priceNumber'
   askSeries.dataFields.valueY = 'askValueY'
-  askSeries.name = 'Sell Suply'
-  askSeries.strokeWidth = 1
+  askSeries.name = 'Sell Supply'
   askSeries.stroke = am4core.color(colors.red)
-  askSeries.fill = askSeries.stroke
-  askSeries.fillOpacity = 0.1
   askSeries.dummyData = {
+    flag: sellSupplyPic,
     description:
       'Shows sell supply of the auction<br/> based on the price and nominated<br/> in the bidding token',
-    path:
-      'M276.58,66.99l44.26,103.44c2.96,6.92,9.48,11.65,16.97,12.33l112.05,10.13c17.93,1.62,25.17,23.93,11.62,35.78l-84.7,74.05c-5.66,4.95-8.15,12.61-6.48,19.95l25,109.7c4,17.55-14.98,31.34-30.43,22.11l-96.6-57.67c-6.46-3.86-14.51-3.86-20.97,0l-96.6,57.67c-15.45,9.23-34.43-4.56-30.43-22.11l25-109.7c1.67-7.33-0.82-15-6.48-19.95l-84.7-74.05c-13.55-11.85-6.3-34.16,11.62-35.78l112.05-10.13c7.49-0.68,14.01-5.41,16.97-12.33l44.26-103.44C246.04,50.44,269.5,50.44,276.58,66.99z',
   }
 
   // New order to be placed
@@ -110,21 +131,16 @@ export const XYChart = (props: XYChartProps): am4charts.XYChart => {
   inputSeries.dataFields.valueX = 'priceNumber'
   inputSeries.dataFields.valueY = 'newOrderValueY'
   inputSeries.name = 'New orders'
-  inputSeries.strokeWidth = 1
   inputSeries.stroke = am4core.color(colors.orange)
-  inputSeries.fill = inputSeries.stroke
-  inputSeries.fillOpacity = 0.1
   inputSeries.dummyData = {
+    flag: newOrderPic,
     description:
       'Shows the new order<br/> that would be placed based<br/> on the current amount and price input',
-    path:
-      'M276.58,66.99l44.26,103.44c2.96,6.92,9.48,11.65,16.97,12.33l112.05,10.13c17.93,1.62,25.17,23.93,11.62,35.78l-84.7,74.05c-5.66,4.95-8.15,12.61-6.48,19.95l25,109.7c4,17.55-14.98,31.34-30.43,22.11l-96.6-57.67c-6.46-3.86-14.51-3.86-20.97,0l-96.6,57.67c-15.45,9.23-34.43-4.56-30.43-22.11l25-109.7c1.67-7.33-0.82-15-6.48-19.95l-84.7-74.05c-13.55-11.85-6.3-34.16,11.62-35.78l112.05-10.13c7.49-0.68,14.01-5.41,16.97-12.33l44.26-103.44C246.04,50.44,269.5,50.44,276.58,66.99z',
   }
 
   // Dotted white line -> shows the Current price, which is the closing price of the auction if
   // no more bids are submitted or cancelled and the auction ends
   const currPriceMockVal = '4.05 DAI'
-
   const priceSeries = chart.series.push(new am4charts.LineSeries())
   priceSeries.dataFields.valueX = 'priceNumber'
   priceSeries.dataFields.valueY = 'clearingPriceValueY'
@@ -132,12 +148,9 @@ export const XYChart = (props: XYChartProps): am4charts.XYChart => {
   priceSeries.strokeWidth = 2
   priceSeries.strokeDasharray = '3,3'
   priceSeries.stroke = am4core.color(colors.white)
-  priceSeries.fill = priceSeries.stroke
-  priceSeries.fillOpacity = 0.1
   priceSeries.dummyData = {
+    flag: currPricePic,
     description: `Shows the current price: <strong style="font-size:14px;">[${currPriceMockVal}]</strong> <br/>This price would be<br/> the closing price of the auction<br/> if no more bids are submitted or cancelled`,
-    path:
-      'M276.58,66.99l44.26,103.44c2.96,6.92,9.48,11.65,16.97,12.33l112.05,10.13c17.93,1.62,25.17,23.93,11.62,35.78l-84.7,74.05c-5.66,4.95-8.15,12.61-6.48,19.95l25,109.7c4,17.55-14.98,31.34-30.43,22.11l-96.6-57.67c-6.46-3.86-14.51-3.86-20.97,0l-96.6,57.67c-15.45,9.23-34.43-4.56-30.43-22.11l25-109.7c1.67-7.33-0.82-15-6.48-19.95l-84.7-74.05c-13.55-11.85-6.3-34.16,11.62-35.78l112.05-10.13c7.49-0.68,14.01-5.41,16.97-12.33l44.26-103.44C246.04,50.44,269.5,50.44,276.58,66.99z',
   }
 
   // Add cursor
@@ -160,72 +173,12 @@ export const XYChart = (props: XYChartProps): am4charts.XYChart => {
   chart.zoomOutButton.icon.strokeWidth = 2
   chart.zoomOutButton.tooltip.text = 'Zoom out'
 
-  // Legend
-  chart.legend = new am4charts.Legend()
-  chart.legend.position = 'bottom'
-  chart.legend.marginBottom = 15
-  chart.legend.marginTop = 10
-  // chart.legend.useDefaultMarker = true
-  // chart.colors.step = 2
-  // /* Remove square from marker template */
-  // const marker = chart.legend.markers.template
-  // marker.disposeChildren()
-  // marker.width = 40
-  // marker.height = 40
-  //
-  // const flag = marker.createChild(am4core.Sprite)
-  // flag.width = 40
-  // flag.height = 40
-  // flag.scale = 0.1
-  // flag.verticalCenter = 'top'
-  // flag.horizontalCenter = 'left'
-  //
-  // flag.propertyFields.fill = 'fill'
-  //
-  // flag.adapter.add('dx', function (dx, target) {
-  //   target.path = target.dataItem.dataContext?.dummyData.path
-  //   return dx
-  // }
-
   chart.legend.labels.template.fill = am4core.color(colors.white)
   chart.legend.itemContainers.template.tooltipHTML =
     '<div>{dataContext.dummyData.description}</div>'
   chart.tooltip.getFillFromObject = false
   chart.tooltip.background.stroke = am4core.color(colors.blue)
   chart.tooltip.background.fill = am4core.color(colors.darkBlue)
-  const marker = chart.legend.markers.template
-  marker.disposeChildren()
-  const flag = marker.createChild(am4core.Sprite)
-  flag.width = 40
-  flag.height = 40
-  flag.scale = 0.1
-  flag.verticalCenter = 'top'
-  flag.horizontalCenter = 'left'
-
-  flag.propertyFields.fill = 'fill'
-  flag.adapter.add('dx', (dx, target) => {
-    target.path = target?.dummyData?.path ?? 0
-    return dx
-  })
-
-  // const dollar = marker.createChild(am4core.Image)
-  // dollar.width = 40
-  // dollar.height = 40
-  // dollar.verticalCenter = 'top'
-  // dollar.horizontalCenter = 'left'
-  chart.legend.useDefaultMarker = true
-  // const marker = chart.legend.markers.template
-  // // markerColumnActiveState.properties.fillOpacity = 0;
-  //
-  // // Add custom image instead
-  // const checkbox = marker.createChild(am4core.Image)
-  // checkbox.width = 23
-  // checkbox.height = 23
-  // checkbox.verticalCenter = 'top'
-  // checkbox.horizontalCenter = 'left'
-  //
-  // checkbox.href ='data:image/svg+xml;base64, PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiBoZWlnaHQ9IjIzcHgiIHZlcnNpb249IjEuMSIgdmlld0JveD0iMCAwIDIzIDIzIiB3aWR0aD0iMjNweCI+PHRpdGxlLz48ZGVzYy8+PGRlZnMvPjxnIGZpbGw9IiMwMDAwMDAiIHN0eWxlPSJwb2ludGVyLWV2ZW50czogbm9uZTsiPjxnIGZpbGw9IiNmZmYiIGZpbGwtb3BhY2l0eT0iMC4xIiBzdHJva2Utb3BhY2l0eT0iMCI+PHJlY3Qgd2lkdGg9IjIzIiBoZWlnaHQ9IjIzIi8+PC9nPjwvZz48L3N2Zz4K'
-
   return chart
 }
 
@@ -264,7 +217,7 @@ export const drawInformation = (props: DrawInformation) => {
   }
 
   const {
-    values: [askPricesSeries, bidPricesSeries, priceSeries],
+    values: [askPricesSeries, inputSeries, bidPricesSeries, priceSeries],
   } = chart.series
 
   priceSeries.dummyData = {
