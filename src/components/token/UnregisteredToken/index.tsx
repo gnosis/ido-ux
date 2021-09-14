7d134df43fb49522b4af3f135e144037b1e37dad
Maria Yablonskaya <mariayablonskaya@MacBook-Pro-Maria.local>
2021-09-14 23:38:58 +0300

    lint reverts && tst upd

--- src/components/token/UnregisteredToken/index.tsx
+++ src/components/token/UnregisteredToken/index.tsx
@@ -48,7 +48,7 @@ export const UnregisteredToken: React.FC<Props> = (props) => {
   const timestamp = React.useMemo(() => Date.now(), [])
   const tooltipId = `tooltip_${symbol}_${size}_${timestamp}`
   const cuttedSymbol = symbol.slice(0, 2)
-  console.log('okok', symbol, cuttedSymbol)
+
   return (
     <Wrapper
       data-for={tooltipId}
