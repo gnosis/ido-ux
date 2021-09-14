fa1686efa451a0b252fd8e7e301d58924ce5f131
Maria Yablonskaya <mariayablonskaya@MacBook-Pro-Maria.local>
2021-09-15 00:23:18 +0300

    fix token display method

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -197,9 +197,7 @@ export function getTokenDisplay(token: Token, chainId: ChainId): string {
 export function getFullTokenDisplay(token: Token, chainId: ChainId): string {
   if (isTokenXDAI(token.address, chainId)) return `XDAI`
   if (isTokenWETH(token.address, chainId)) return `ETH`
-  return (
-    token?.symbol?.slice(0, 7) || token?.name?.slice(0, 7) || token?.address.slice(0, 7) || '🤔'
-  )
+  return token?.symbol || token?.name || token?.address || '🤔'
 }
 
 export function isTokenXDAI(tokenAddress?: string, chainId?: ChainId): boolean {
